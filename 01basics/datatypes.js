e7d2aff594a60a1ce813bfbbd05d8b2ea8848509
ankit kumar <103094580+Ankitkumar-git@users.noreply.github.com>
2024-06-26 17:56:25 +0000

    datatypes and variables

---
+++ 01basics/datatypes.js
@@ -0,0 +1,10 @@
+'use strict'//treats all js code as new version
+/*string is always written withen double quotes or 
+single quotes*/
+//boolean= true /false
+//biginnt= for large int valuesdaa
+let ankitage=22
+console.log(typeof ankitage )
+console.log(typeof 'ankitage' )
+console.log(typeof 123456789012345678.1234567890 )
+console.log(typeof true )
---
+++ 01basics/variables.js
@@ -0,0 +1,11 @@
+const enrollmentnumber =380
+let name = "ankit_kumar"
+branch= "c.s.e"
+qualification='undergraduation'
+console.table([enrollmentnumber,name,branch,qualification])
+
+//variables can be written directly without using any keywords like
+
+//let ,var but for code readability we should use let 
+
+/*var should be strictly avoided*/
