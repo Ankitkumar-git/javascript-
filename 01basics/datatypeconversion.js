cee8a7eab9a4c41b18781504be10c75d4bd7d996
ankit kumar <103094580+Ankitkumar-git@users.noreply.github.com>
2024-06-27 18:17:19 +0000

    number conversion

---
+++ 01basics/datatypeconversion.js
@@ -0,0 +1,14 @@
+let score ='ankit'
+console.log(typeof(score))
+console.log(typeof score)
+ 
+// here we will convert in number we can achieve it by in any form of data types 
+let valueInNumber = Number(score) 
+console.log( typeof valueInNumber )
+console.log( valueInNumber)// here this line is used to show 
+//what value is in the 'valueinnumber' which is displaying nan(not a number) value
+// it is showing type of the value which is number 
+
+
+
+
